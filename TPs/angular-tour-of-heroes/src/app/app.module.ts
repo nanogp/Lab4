26bcba39dbe25b3be22d9344431aaf2bc9bd63b8
Nano <nano@iMac.local>
2019-08-25 18:28:20 -0300

    servicios

--- TPs/angular-tour-of-heroes/src/app/app.module.ts
+++ TPs/angular-tour-of-heroes/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './componentes/heroes/heroes.component';
 import { HeroDetailComponent } from './componentes/hero-detail/hero-detail.component';
+import { MessagesComponent } from './componentes/messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
---
+++ TPs/angular-tour-of-heroes/src/app/servicios/hero.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Hero } from "../clases/hero";
+import { HEROES } from "../clases/mock-heroes";
+import { Observable, of } from 'rxjs';
+import { MessageService } from "./message.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+  }
+}
